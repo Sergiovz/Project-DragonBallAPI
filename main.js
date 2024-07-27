02fbfaefee4dafc3165b16b17bad248f4aaf60eb
SergioVZ <sergiovaldez599@gmail.com>
2024-07-27 10:37:15 -0300

    Nueva Rama Creada y funcionalidad de buscador agregada

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const txtCharacter = document.getElementById('txtCharacter');
 const containerCards = document.getElementById('containerCards');
 const URL1 = 'https://dragonball-api.com/api/Characters';
+const URL2 = 'https://dragonball-api.com/api/Characters?name=';
 
 
 const getApi = async (URL) => {
     const response = await fetch(URL);
     const data = await response.json();
     console.log(data);
-    return data.items;
+    return data;
 };
 
 const createCards = ( character ) => {
@@ -34,11 +35,30 @@ const createCards = ( character ) => {
     card.appendChild(containerDescription);
 
     containerCards.appendChild(card);
+
+            //Example of a model
+    // <div class="card-character">
+    //     <img src="..." alt="...">
+    //     <div class="description-card">
+    //         <h2>name</h2>
+    //         <p>gender</p>
+    //     </div>
+    // </div>
+
 };
 
 const generateAllCharacters = async () => {
-    const dataItems = await getApi(URL1);
+    const data = await getApi(URL1);
+    dataItems = data.items;
     dataItems.map((character) => createCards(character));
 };
 
+const getCharacterByName = async (event) => {
+    containerCards.innerHTML = "";
+    const data = await getApi(URL2+event.target.value);
+    console.log(data);
+    data.map( character => createCards(character));
+};
+
 window.addEventListener('DOMContentLoaded', generateAllCharacters);
+txtCharacter.addEventListener('keyup', getCharacterByName);
